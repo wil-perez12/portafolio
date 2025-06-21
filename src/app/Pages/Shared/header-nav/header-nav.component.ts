76b13505b9a13dcba66f224e25a79bd4eac2ce7c
ing Wilnel <perezwilnel12@gmail.com>
2025-06-21 19:09:47 -0400

    talwind css instaled

---
+++ src/app/Pages/Shared/header-nav/header-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav',
+  imports: [],
+  templateUrl: './header-nav.component.html',
+  styleUrl: './header-nav.component.css'
+})
+export class HeaderNavComponent {
+
+}
