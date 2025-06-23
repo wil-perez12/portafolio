bf28bd9f844014a0743a3775dc4f0926d5469895
ing Wilnel <perezwilnel12@gmail.com>
2025-06-23 10:25:55 -0400

    building nav bar

--- src/app/Pages/Shared/header-nav/header-nav.component.ts
+++ src/app/Pages/Shared/header-nav/header-nav.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-nav',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-nav.component.html',
   styleUrl: './header-nav.component.css'
 })
 export class HeaderNavComponent {
+  isOpen = false;
 
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderNavComponent } from "./Pages/Shared/header-nav/header-nav.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
