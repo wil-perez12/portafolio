22a08a3d84c087a01b1aeea7fe7955bb31b3cd9d
ing Wilnel <perezwilnel12@gmail.com>
2025-06-25 17:22:48 -0400

    structure done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderNavComponent } from "./Pages/Shared/header-nav/header-nav.component";
+import { InicioComponent } from './Pages/Shared/inicio/inicio.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderNavComponent],
+  imports: [HeaderNavComponent,InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
